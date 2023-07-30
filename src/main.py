88a64b19e0d6e576d20b25c8fd2fa4325a5ed200
soheeparklee <soheeparklee@gmail.com>
2023-07-30 19:07:11 +0900

    add post
    add post and js

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, Form
 from fastapi.staticfiles import StaticFiles
+from typing import Annotated
 
 app= FastAPI()
-app.mount("/", StaticFiles(directory="frontend", html= True), name="frontend")
+
+@app.post("/items")
+def create_item(image:UploadFile, 
+                title:Annotated[str, Form()], 
+                price: Annotated[int, Form()], 
+                description: Annotated[str, Form()], 
+                place: Annotated[str, Form()]):
+
+
+
+    app.mount("/", StaticFiles(directory="frontend", html= True), name="frontend")
