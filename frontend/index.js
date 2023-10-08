d4066c676b12603500202404659cad0f2692b2df
soheeparklee <soheeparklee@gmail.com>
2023-10-09 00:54:21 +0900

    GET items
    - import json response, response encoder
    - to bring all data of db => fetchall
     - to bring column name => con.rowfacrory= sqlite3.Row

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,9 @@
 
 
-async const fetchList= () => {
-    const res= await fetch('/items')
+const fetchList= async () => {
+    const res= await fetch('/items');
+    const data= await res.json();
+    console.log(data);
 }
+
+fetchList();
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, UploadFile, Form
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
 import sqlite3
+#for GET, need to import JSONresponse
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 #sqlite settings
 con = sqlite3.connect("db.db", check_same_thread=False)
@@ -33,7 +36,18 @@ async def create_item(image:UploadFile,
     print(image, title, price, description, place, insertat)
     return "200"
 
-
+@app.get("/items")
+async def get_items():
+    #bring column name(각 값들이 무엇을 의미하는지 알기 위해)
+    con.row_factory= sqlite3.Row
+    #bring data, in form of array
+    cur= con.cursor()
+    rows= cur.execute(f"""
+                    SELECT * from items;
+                    """).fetchall()
+    #add (dict) to make response neatly organized
+    return JSONResponse(jsonable_encoder(dict(row) for row in rows))
+    
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
 
