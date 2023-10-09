38d7ee3f9f3db97579c2c87a196df910cde23b8b
soheeparklee <soheeparklee@gmail.com>
2023-10-09 19:08:36 +0900

    feat: sha256, password check
    - hide id, password using SHA256
    - check if password is the same(비밀버호 확인)
    - if password does not match, pop up a message

--- frontend/signup.js
+++ frontend/signup.js
@@ -1,12 +1,45 @@
 const form= document.querySelector("#signup-form");
 
+
+//double check password
+const checkPassword= () =>{
+    const formData= new FormData(form);
+    const password1= formData.get("password");
+    const password2= formData.get("password2");
+    if (password1 === password2) {
+        return true;
+    } else return false;
+
+}
+
 const handleSubmit= async (event) =>{
     event.preventDefault();
-    const body= new FormData(form);
+    const formData= new FormData(form);
+    //change form data password using sha256
+    //formdata의 get써서 password가져온 다음에, sha256 사용해서 암호화
+    const sha256Password= sha256(formData.get("password"));
+    formData.set("password", sha256Password);
+
+    //password 일치하지 않았을 때 보여지는 div
+    const div= document.querySelector("#passwordCheck");
+
+    //check password
+    if (checkPassword()){
+        //hide message
+        div.innerText= "";
+
+    //POST on server
     const res= await fetch("/signup",{
         method: "post",
-        body: body,
+        body: formData,
+
+    
     });
-}
+    } else{
+        div.innerText= "비밀번호가 일치하지 않습니다. "
+    }
+
+
+};
 
 form.addEventListener("submit", handleSubmit);
