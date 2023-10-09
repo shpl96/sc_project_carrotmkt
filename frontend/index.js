8472813c5742cc8b0702ca1a9a9d7544573b0929
soheeparklee <soheeparklee@gmail.com>
2023-10-09 17:14:44 +0900

    create HTML from JS, add time info
    - createElement 사용해서 아이템 추가할 때마다 html에 추가되도록 한다.
    - "~분 전" 보여지기 위해서 calcTime
    - 미국 시간으로 계산되니까 9시간 빼줘야 함
    - 서버에서 받아온 시간에서 지금 시간 빼야함
    - 가장 마지막으로 쓴게 가장 위로 올라왔으면 좋겠으니까 array의 순서를 바꿀때는 reverse

--- frontend/index.js
+++ frontend/index.js
@@ -1,14 +1,67 @@
+//add time meta info
+const calcTime = (timestamp) =>{
+    const curTime= new Date().getTime() -9*60*60*1000;
+    const time= new Date(curTime - timestamp);
+    const hour= time.getHours();
+    const min= time.getMinutes();
+    const sec= time.getSeconds();
+
+    if (hour>0) return `${hour}시간 전`
+    else if (min>0) return `${min}분 전`
+    else if (sec>=0) return `${sec}초 전`
+    else return "방금 전";
+
+}
+
 //with the data, send it to frontend HTML to show
 const renderData= (data) =>{
     //html에서 main안에 div만들어 가져온 정보 추가할거임
-    const main= document.querySelector("main";)
+    const main = document.querySelector("main");
     
     //배열 내부의 값을 하나씩 돌면서 다음 명령을 수행 => for each
-    data.forEach((obj) => {
-        const div= document.createElement("div");
-        div.innerText= obj.title;
-        main.appendChild(div);
+    // 가장 처음 올린 것이 밑으로, 마지막에 올린 것이 위로 => reverse
+    data.reverse().forEach((obj) => {
+        //add div to HTML >> get title from server using for each >> append div to main
+        const mainBoxList= document.createElement("div");
+        mainBoxList.className = "main-box-list";
+        //create image box
+        const boxImg= document.createElement("div");
+        boxImg.className= "box-img";
+        const img= document.createElement("img");
+        img.src= "assets/image.svg";
+
+        //create description box
+        const boxDesc= document.createElement("div");
+        boxDesc.className = "box-desc";
+
+        const descTitle= document.createElement("div");
+        descTitle.className = "desc-title";
+        descTitle.innerText= obj.title;
+
+        const descMeta= document.createElement("div");
+        descMeta.className = "desc-meta";
+        //add time info 
+        descMeta.innerText= obj.place + " " + calcTime(obj.insertat);
+
+        
+
+        const descPrice= document.createElement("div");
+        descPrice.className = "decs-price";
+        descPrice.innerText= obj.price;
+
+        //append img
+        mainBoxList.appendChild(boxImg);
+        boxImg.appendChild(img);
+        //append decsription
+        mainBoxList.appendChild(boxDesc);
+        boxDesc.appendChild(descTitle);
+        boxDesc.appendChild(descMeta);
+        boxDesc.appendChild(descPrice);
+        //append everything to main
+        main.appendChild(mainBoxList);
         });
+
+
 }
 
 //GET data from server
