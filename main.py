f73bee100830ff515b60b2cb716288072224be56
soheeparklee <soheeparklee@gmail.com>
2023-10-09 21:16:31 +0900

    add: db file add
    sign up info added to db users table

--- main.py
+++ main.py
@@ -60,9 +60,15 @@ async def get_img(item_id):
 #signup page
 @app.post("/signup")
 def signup(id: Annotated[str, Form()],
-           password: Annotated[str, Form()]
-           ):
-    print(id, password)
+            password: Annotated[str, Form()],
+            name: Annotated[str, Form()],
+            email: Annotated[str,Form()]
+            ):
+    cur.execute(f"""
+                    INSERT INTO users(id, name, email, password)
+                    VALUES('{id}', '{name}', '{email}', '{password}')
+                    """)
+    con.commit()
     return "200"
 
 
