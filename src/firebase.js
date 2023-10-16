dd3e398648f6b1e5c61861ef986fb481a127ccac
soheeparklee <soheeparklee@gmail.com>
2023-10-17 01:09:29 +0900

    feat: add env to hide data

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBr-3mqSS1P6RjeptqUTTkhUZ9JbbAsM1U",
+  authDomain: "sc-carrot-market.firebaseapp.com",
+  databaseURL: "https://sc-carrot-market-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sc-carrot-market",
+  storageBucket: "sc-carrot-market.appspot.com",
+  messagingSenderId: "145483233478",
+  appId: "1:145483233478:web:aaa49d3c41e1fea4f1bfc7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
